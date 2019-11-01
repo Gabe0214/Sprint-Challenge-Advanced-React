4e7c2e4a22f56ce1ede7a0c0178cf68f2dbdd57a
Gabriel Anguiano <anguianog97@gmail.com>
2019-11-01 08:11:32 -0700

    women list to fetch data set up

---
+++ client/src/components/WomenList.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+class WomenList extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            players: []
+        }
+    }
+
+
+
+    render(){
+        return(
+            <div>
+                <h2>Testing</h2>
+            </div>
+        )
+    }
+}
+
+export default WomenList; 
