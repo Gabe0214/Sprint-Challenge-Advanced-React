f3487fe92a93dfae2a48c8e8e9c709970a15ee44
Gabriel Anguiano <anguianog97@gmail.com>
2019-11-01 10:53:09 -0700

    added some props

--- client/src/components/PlayerCard.js
+++ client/src/components/PlayerCard.js
@@ -6,7 +6,7 @@ const PlayerCard = props => {
     return (
         <div className='App'>
          <h2>{props.player.name }</h2>
-         <p>{props.player.country}</p>
+         <p> Country: {props.player.country}</p>
         </div>
     )
 }
